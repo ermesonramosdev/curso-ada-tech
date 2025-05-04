0ccafc1b374d6e0108f8204cbe22963e71fe7dcc
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-05-03 21:38:56 -0300

    Componentização funcional

--- React/first-project/src/components/Article/Article.jsx
+++ React/first-project/src/components/Article/Article.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import './style.css';
 
-export class Article extends React.Component {
-  render() {
-    return (
-      <article id="article">
-        
-        <img src={this.props.thumbnail} alt={this.props.title} />
-        <div className="article-infos">
-          <h2> {this.props.title} </h2>
-
-          <h3> {this.props.provider} </h3>
-
-          <p> {this.props.description} </p>
-
-        </div>
-      </article>
-    )
-  }
+export function Article( {thumbnail, title, provider, description} ) {
+  return (
+    <article id="article">
+      <img src={thumbnail} alt={title} />
+      <div className="article-infos">
+        <h2> {title} </h2>
+        <h3> {provider} </h3>
+        <p> {description} </p>
+      </div>
+    </article>
+  )
 }
 
