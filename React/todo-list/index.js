591c4817869c3a7f01d92730e3fec5626342cb12
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-05-04 17:58:42 -0300

    Criação do componente Header

---
+++ React/todo-list/index.js
@@ -0,0 +1,11 @@
+/*
+    ! Componentes Funcionais e Nova forma de estilo
+        * O que são componentes funcionais: 
+        -> Componentes funcionais são funções que retorna HTML.
+
+        * React + TypeScript
+         ? : React.FC
+            -> Determina que o componente se torna funcional.
+
+
+*/
---
+++ React/todo-list/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import './styles.css';
+export const Header: React.FC = () => {
+    return(
+       <header id="header">
+        <div>
+            <h1>
+                MyTodo
+            </h1>
+            <span>Bem Vindo, Ermeson!</span>
+        </div>
+        <div>
+            { /* Cards */ }
+        </div>
+       </header>
+    );
+}
