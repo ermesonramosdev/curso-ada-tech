eb6889df81730a33828cf6bfbfe08b5c53e593f2
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-05-13 21:34:03 -0300

    A utilização do UseState

--- React/todo-list/index.js
+++ React/todo-list/index.js
@@ -10,5 +10,20 @@
         * CSS Modules 
         ->  Vai criar uma estilização unica para meu componente para não ter conflitos entre as classes repetidas de outro componente.
 
+    ! React Funcional II - Hook de estado 
+        * O que são os Hooks:
+        -> Ele vai permite acessar os estados e ciclo de vida do componente.
+
+        * Regras do Hooks:
+        -> Chame apenas os Hooks no nível mais alto do componente(Chama logo no escopo inicial). Não chame Hooks dentro de loops condições ou funções aninhadas.
+
+        -> Apenas chama Hooks de componentes funcionais. Não chame Hooks de funções JavaScript comuns.
+
+        * Como indentificar um Hooks:
+        -> Sempre vai começar com um use[...].
+
+        * Hooks de Estado:
+        -> Estado: O mesmo conceito que vimos nos componentes em classe.
+
 
 */
